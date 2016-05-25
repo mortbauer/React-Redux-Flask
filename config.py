90282c91d9ac3233f0f71620299583945b6e8d48
Martin Ortbauer <martin.ortbauer@at.schindler.com>
2016-05-25 17:31:15 +0200

    make use of inheritance :change:

--- config.py
+++ config.py
@@ -1,19 +1,17 @@
-import os
-
-from setup import basedir
-
 
 class BaseConfig(object):
     SECRET_KEY = "SO_SECURE"
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
-class TestingConfig(object):
+class TestingConfig(BaseConfig):
     """Development configuration."""
     TESTING = True
     DEBUG = True
     WTF_CSRF_ENABLED = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     DEBUG_TB_ENABLED = True
     PRESERVE_CONTEXT_ON_EXCEPTION = False
+
+class ProductionConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/yourdb'
